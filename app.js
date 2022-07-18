0e35eee0df1fc8d22c2a823f99676a18fac20c65
Maitri <trivedimaitri2000@gmail.com>
2022-07-18 19:17:10 +0530

    port comment

--- app.js
+++ app.js
@@ -154,9 +154,9 @@ app.get("/about", function (req, res) {
 });
 
 let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+// if (port == null || port == "") {
+  
+// }
 
 app.listen(port, function () {
   console.log("Server started successfully on port");
