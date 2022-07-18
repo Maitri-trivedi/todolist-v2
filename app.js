85ad32c3a841bd0eba9e679221cd83dab9fb8f11
Maitri <trivedimaitri2000@gmail.com>
2022-07-18 19:12:17 +0530

    changes to port and start of package json

--- app.js
+++ app.js
@@ -157,9 +157,7 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port);
-
 
 app.listen(port, function () {
-  console.log("Server started successfully on port" + port);
+  console.log("Server started successfully on port");
 });
